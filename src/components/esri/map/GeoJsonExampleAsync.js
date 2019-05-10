84512f1480151336b23a953a131d5c6e3631bdd2
Will Carter <will.carter@fergusllc.com>
2019-05-10 10:43:14 -0600

    changed api call to 127.0.0.1

--- src/components/esri/map/GeoJsonExampleAsync.js
+++ src/components/esri/map/GeoJsonExampleAsync.js
@@ -197,7 +197,7 @@ class GeoJsonExampleAsync extends Component {
           
       const box = await this.toLatLngExtent(this.view.extent);
 
-      const url = `http://104.236.16.91:8670/h3/${box.top_left}/${box.bottom_left}/${box.bottom_right}/${box.top_right}/${this.view.zoom}`;
+      const url = `http://127.0.01:8670/h3/${box.top_left}/${box.bottom_left}/${box.bottom_right}/${box.top_right}/${this.view.zoom}`;
       console.log(url);
 
       const geoJsonLayer = new GeoJSONLayer({
